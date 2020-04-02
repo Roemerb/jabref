d746f7434f7b1e3d766ad6c7db62538afed622ea
Amin <amin_chihab@hotmail.com>
2020-04-02 18:38:08 +0200

    Update FileUtil.java

--- src/main/java/org/jabref/logic/util/io/FileUtil.java
+++ src/main/java/org/jabref/logic/util/io/FileUtil.java
@@ -108,7 +108,12 @@ public class FileUtil {
     public static List<String> uniquePathSubstrings(List<String> paths) {
         List<Stack<String>> stackList = new ArrayList<>(paths.size());
         // prepare data structures
-        prepareUniquePathSubstrings(paths);
+        for (String path : paths) {
+            List<String> directories = Arrays.asList(path.split(Pattern.quote(File.separator)));
+            Stack<String> stack = new Stack<>();
+            stack.addAll(directories);
+            stackList.add(stack);
+        }
 
         List<String> pathSubstrings = new ArrayList<>(Collections.nCopies(paths.size(), ""));
 
